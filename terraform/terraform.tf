9b7bc9b19b0c8f228107c43aab0e421e83e94f8f
rudra89-0 <rodriguesbrendan644@gmail.com>
2025-06-06 20:46:17 +0530

    4758

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0.0-beta3"
+      version = "~> 5.25.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.7.2"
+      version = "~> 3.5.1"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.1.0"
+      version = "~> 4.0.4"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.7"
+      version = "~> 2.3.2"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.37.1"
+      version = "~> 2.23.0"
     }
   }
 
@@ -32,7 +32,7 @@ terraform {
     region = "ap-south-1"
   }
 
-  required_version = "~> 1.1.7"
+  required_version = "~> 1.6.3"
 }
 ##
 ##
